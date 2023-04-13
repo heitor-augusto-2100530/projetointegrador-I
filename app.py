779af803b8e53c3758b56d9f0e330ea612bab06c
unknown <vitor.ozols@cws.digital>
2023-04-12 21:14:11 -0300

    HTML Produto e HTML Categoria

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index_html():
     return render_template("index.html")
+
+@app.route("/product")
+def product_html():
+    return render_template("product.html")
+
+@app.route("/category")
+def category_html():
+    return render_template("category.html")
