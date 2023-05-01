9dc56797904793b9af4880d84436fe83e84f3b40
unknown <vitor.ozols@cws.digital>
2023-05-01 20:21:02 -0300

    Integralçao com banco MySQL, vinculo dos dados com o front, cadastros com exemplos

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
 from flask import Flask
 from flask import render_template
+from flask_mysqldb import MySQL
+from flask import request
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'tws_bd.mysql.dbaas.com.br'
+app.config['MYSQL_USER'] = 'tws_bd'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'tws_bd'
+mysql = MySQL(app)
 
 @app.route("/")
 def index_html():
-    return render_template("index.html")
-
-@app.route("/product")
-def product_html():
-    return render_template("product.html")
+    query = '''select * from pi_category'''
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return render_template("index.html", categorias=data)
 
 @app.route("/category")
 def category_html():
-    return render_template("category.html")
+    category_id = request.args.get('id')
+    category_name = request.args.get('name')
+    query_skus = f'''SELECT id, name, image from pi_sku where category_id = {category_id}'''
+    cur = mysql.connection.cursor()
+    cur.execute(query_skus)
+    data = cur.fetchall()
+    cur.close()
+    return render_template("category.html", skus=data, name=category_name)
+
+@app.route("/product")
+def product_html():
+    sku_id = request.args.get('id')
+    query_sku = f'''SELECT name, description, image from pi_sku where id = {sku_id}'''
+    cur = mysql.connection.cursor()
+    cur.execute(query_sku)
+    data = cur.fetchone()
+    cur.close()
+    return render_template("product.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
